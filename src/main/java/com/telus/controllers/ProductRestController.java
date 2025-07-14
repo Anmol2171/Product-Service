d6d447c33c1743695e3b4dfc2dba34f8789b8ec3
Anmol Singh Tomar <anmol.tomar@telusinternational.com>
2025-07-14 21:21:38 +0530

    updated code

--- src/main/java/com/telus/controllers/ProductRestController.java
+++ src/main/java/com/telus/controllers/ProductRestController.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/products")
@@ -13,6 +15,12 @@ public class ProductRestController {
 	public ResponseEntity<String> getProductInfo(){
 		return ResponseEntity.ok("Redmi 12 5g");
 	}
+	
+	@GetMapping("/laptop")
+	public ResponseEntity<String> getMethodName() {
+		return ResponseEntity.ok("Dell, Intel Core i7");
+	}
+	
 	
 
 }
